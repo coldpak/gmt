f83788e130b17cc53bd154a20d7e262a2efa3b1f
Chan Park <chanpark0202@gmail.com>
2026-04-07 15:10:25 +0900

    fix: randomize tip per session instead of daily rotation

--- modules/tips.sh
+++ modules/tips.sh
@@ -5,14 +5,14 @@ _gmt_tips_render() {
   local tip_count=${#L_TIPS[@]}
   if (( tip_count == 0 )); then return; fi
 
-  local day_of_year=""
-  day_of_year="$(date +%-j)"
+  local rand_val=""
+  rand_val="$RANDOM"
 
   local idx
   if [[ -n "$ZSH_VERSION" ]]; then
-    idx=$(( (day_of_year % tip_count) + 1 ))
+    idx=$(( (rand_val % tip_count) + 1 ))
   else
-    idx=$(( day_of_year % tip_count ))
+    idx=$(( rand_val % tip_count ))
   fi
 
   printf "\n  💡 ${C_DIM}%s:${C_RESET} %s\n" "$L_TIPS_TITLE" "${L_TIPS[$idx]}"
